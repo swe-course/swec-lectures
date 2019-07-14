6ff108c403aac7437ce733a1509f4890e4b8c2fe
Vladyslav Kurmaz <vladislav.kurmaz@gmail.com>
2019-07-14 11:49:49 +0300

    Create install-plugins.sh

---
+++ 02-continuous-integration/02.04-jenkins/install-plugins.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo ---- Installing Jenkins plugins ----
+
+crumb=$(curl -s -u "admin:${1}" "http://localhost:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,\":\",//crumb)")
+echo ${crumb}
+
+plugins=(ghprb sonar pipeline-utility-steps http_request pipeline-maven)
+for plugin in "${plugins[@]}"
+do
+  echo ---- Installing ${plugin} ----
+  curl -s -o /dev/null -w "%{http_code}" POST \
+    --user admin:admin \
+    -H "${crumb}" \
+    --data "<jenkins><install plugin='${plugin}@latest'/></jenkins>" \
+    --header 'Content-Type: text/xml' \
+    http://localhost:8080/pluginManager/installNecessaryPlugins
+  echo
+done
